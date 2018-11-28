39a2a3fe30616d545e432a9c87e26c03bff7b65b
ali <alishaibu2002@gmail.com>
2018-11-28 23:02:07 +0100

    chore(setup-redux) setup reduc [Finishes #162276352]

---
+++ src/root.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import AxiosConfig from './config/axiosConfig';
+
+const enhancers = composeWithDevTools({});
+const state = {
+  auth: {
+    authenticated: localStorage.getItem('token'),
+    user: JSON.parse(localStorage.getItem('user')),
+    ownProfile: JSON.parse(localStorage.getItem('ownProfile')),
+  },
+};
+const API = new AxiosConfig(state.auth.authenticated);
+
+export default ({ children, initialState = { ...state } }) => {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    enhancers(applyMiddleware(thunk.withExtraArgument(API))),
+  );
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
+};
